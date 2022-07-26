031dd756a37b35f1b51e3970a268a20838059fe2
ashut <ashutosh.jaiswal@appsinvo.com>
2022-07-26 12:47:57 +0530

    1.Changes According to Feedback.

--- app/src/main/java/app/gunjan/activities/SplashActivity.kt
+++ app/src/main/java/app/gunjan/activities/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import app.gunjan.R
 import app.gunjan.utill.FCSharedPreferances
 import java.util.*
@@ -16,6 +17,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
     override fun onResume() {
